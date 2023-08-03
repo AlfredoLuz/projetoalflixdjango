b439e82e9125b67e032bcd8e826d0abe3b4da276
AlfredoLuz <alfredo.d.luz@gmail.com>
2023-08-03 19:01:24 -0300

    update 5

--- alflix/settings.py
+++ alflix/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -84,9 +86,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-import dj_database_url
-import os
  
 DATABASE_URL = os.getenv("DATABASE_URL")
 if DATABASE_URL:
