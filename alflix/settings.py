6c9c5b9075a91fc59a5230a0ab0d52eafc1f4cb9
AlfredoLuz <alfredo.d.luz@gmail.com>
2023-08-03 18:16:58 -0300

    debug para True

--- alflix/settings.py
+++ alflix/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2rq9jkgd(ay+(r^g(-6rop0tt*v_pdq0y4+ifr9i^pr4i+m4c@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
